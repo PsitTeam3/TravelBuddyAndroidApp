c810176abc5d07b5ce6820cb495e4e6f94377218
Josef Erben <josef@erben.io>
2017-03-04 16:27:10 +0100

    Initial commit

---
+++ app/src/test/java/group3/psit3/zhaw/ch/travelbuddy/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package group3.psit3.zhaw.ch.travelbuddy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
