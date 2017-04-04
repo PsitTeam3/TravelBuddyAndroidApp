992d9a7adbd20970b897f061b605d116e17f362f
Josef Erben <josef@erben.io>
2017-04-04 19:33:52 +0200

    Introduce lombok to reduce POJO clutter.

--- app/src/main/java/group3/psit3/zhaw/ch/travelbuddy/model/Tour.java
+++ app/src/main/java/group3/psit3/zhaw/ch/travelbuddy/model/Tour.java
@@ -1,44 +1,8 @@
 package group3.psit3.zhaw.ch.travelbuddy.model;
 
-public class Tour {
+import lombok.Data;
+
+public @Data
+class Tour {
     private String name, description, detailDescription, thumbnailUrl;
-
-    public Tour() {
-    }
-
-    public Tour(String title) {
-        this.name = title;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String title) {
-        this.name = title;
-    }
-
-    public String getThumbnailUrl() {
-        return thumbnailUrl;
-    }
-
-    public void setThumbnailUrl(String thumbnailUrl) {
-        this.thumbnailUrl = thumbnailUrl;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDetailDescription() {
-        return this.detailDescription;
-    }
-
-    public void setDetailDescription(String detailDescription) {
-        this.detailDescription = detailDescription;
-    }
 }
